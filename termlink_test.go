0da664c936d42de22d2e8d1b78fe9771bbab69e8
Skyascii <savioxavier112@gmail.com>
2022-06-04 18:26:38 +0400

    chore: use input/expected syntax for testing

--- termlink_test.go
+++ termlink_test.go
@@ -10,64 +10,84 @@ import (
 
 func TestBasicLink(t *testing.T) {
 	if termlink.SupportsHyperlinks() {
-		assert.Equal(t, termlink.Link("Hello", "https://google.com"), "\x1b]8;;https://google.com\aHello\x1b]8;;\a\x1b[m")
+		input := termlink.Link("Hello", "https://google.com")
+		expected := "\x1b]8;;https://google.com\aHello\x1b]8;;\a\x1b[m"
+		assert.Equal(t, input, expected)
 	} else {
-		assert.Equal(t, termlink.Link("Hello", "https://google.com"), "Hello (\u200Bhttps://google.com)\u001b[m")
+		input := termlink.Link("Hello", "https://google.com")
+		expected := "Hello (\u200Bhttps://google.com)\u001b[m"
+		assert.Equal(t, input, expected)
 	}
 }
 
 func TestColorLink(t *testing.T) {
 	if termlink.SupportsHyperlinks() {
-		assert.Equal(t, termlink.ColorLink("Hello", "https://google.com", "red"),
-			"\x1b]8;;https://google.com\a\x1b[31mHello\x1b]8;;\a\x1b[m")
+		input := termlink.ColorLink("Hello", "https://google.com", "red")
+		expected := "\x1b]8;;https://google.com\a\x1b[31mHello\x1b]8;;\a\x1b[m"
+		assert.Equal(t, input, expected)
 	} else {
-		assert.Equal(t, termlink.ColorLink("Hello", "https://google.com", "red"),
-			"\x1b[31mHello (\u200bhttps://google.com)\x1b[m")
-
+		input := termlink.ColorLink("Hello", "https://google.com", "red")
+		expected := "\x1b[31mHello (\u200bhttps://google.com)\x1b[m"
+		assert.Equal(t, input, expected)
 	}
 }
 
 func TestColorsPackageLink(t *testing.T) {
 	if termlink.SupportsHyperlinks() {
-		// Check if color.Cyan(termlink.Link("Hello", "https://google.com")) will print the output to the terminal
-		assert.Equal(t, color.New(color.FgCyan).SprintFunc()(termlink.Link("Hello", "https://google.com")), "\x1b[36m\x1b]8;;https://google.com\aHello\x1b]8;;\a\x1b[m\x1b[0m")
+		input := color.New(color.FgCyan).SprintFunc()(termlink.Link("Hello", "https://google.com"))
+		expected := "\x1b[36m\x1b]8;;https://google.com\aHello\x1b]8;;\a\x1b[m\x1b[0m"
+		assert.Equal(t, input, expected)
 	} else {
-		assert.Equal(t, color.New(color.FgCyan).SprintFunc()(termlink.Link("Hello", "https://google.com")), "\x1b[36mHello (\u200bhttps://google.com)\x1b[m\x1b[0m")
+		input := color.New(color.FgCyan).SprintFunc()(termlink.Link("Hello", "https://google.com"))
+		expected := "\x1b[36mHello (\u200bhttps://google.com)\x1b[m\x1b[0m"
+		assert.Equal(t, input, expected)
 	}
 }
 
 func TestBoldLink(t *testing.T) {
 	if termlink.SupportsHyperlinks() {
-		assert.Equal(t, termlink.ColorLink("Hello", "https://google.com", "bold"),
-			"\x1b]8;;https://google.com\a\x1b[1mHello\x1b]8;;\a\x1b[m")
+		input := termlink.ColorLink("Hello", "https://google.com", "bold")
+		expected := "\x1b]8;;https://google.com\a\x1b[1mHello\x1b]8;;\a\x1b[m"
+		assert.Equal(t, input, expected)
 	} else {
-		assert.Equal(t, termlink.ColorLink("Hello", "https://google.com", "bold"), "\x1b[1mHello (\u200bhttps://google.com)\x1b[m")
+		input := termlink.ColorLink("Hello", "https://google.com", "bold")
+		expected := "\x1b[1mHello (\u200bhttps://google.com)\x1b[m"
+		assert.Equal(t, input, expected)
 	}
 }
 
 func TestItalicsLink(t *testing.T) {
 	if termlink.SupportsHyperlinks() {
-		assert.Equal(t, termlink.ColorLink("Hello", "https://google.com", "italic"),
-			"\x1b]8;;https://google.com\a\x1b[3mHello\x1b]8;;\a\x1b[m")
+		input := termlink.ColorLink("Hello", "https://google.com", "italic")
+		expected := "\x1b]8;;https://google.com\a\x1b[3mHello\x1b]8;;\a\x1b[m"
+		assert.Equal(t, input, expected)
 	} else {
-		assert.Equal(t, termlink.ColorLink("Hello", "https://google.com", "italic"), "\x1b[3mHello (\u200bhttps://google.com)\x1b[m")
+		input := termlink.ColorLink("Hello", "https://google.com", "italic")
+		expected := "\x1b[3mHello (\u200bhttps://google.com)\x1b[m"
+		assert.Equal(t, input, expected)
 	}
 }
 
 func TestItalicsBoldLink(t *testing.T) {
 	if termlink.SupportsHyperlinks() {
-		assert.Equal(t, termlink.ColorLink("Hello", "https://google.com", "bold italic"),
-			"\x1b]8;;https://google.com\a\x1b[1;3mHello\x1b]8;;\a\x1b[m")
+		input := termlink.ColorLink("Hello", "https://google.com", "bold italic")
+		expected := "\x1b]8;;https://google.com\a\x1b[1;3mHello\x1b]8;;\a\x1b[m"
+		assert.Equal(t, input, expected)
 	} else {
-		assert.Equal(t, termlink.ColorLink("Hello", "https://google.com", "bold italic"), "\x1b[1;3mHello (\u200bhttps://google.com)\x1b[m")
+		input := termlink.ColorLink("Hello", "https://google.com", "bold italic")
+		expected := "\x1b[1;3mHello (\u200bhttps://google.com)\x1b[m"
+		assert.Equal(t, input, expected)
 	}
 }
 
 func TestColorItalicsBoldLink(t *testing.T) {
 	if termlink.SupportsHyperlinks() {
-		assert.Equal(t, termlink.ColorLink("Hello", "https://google.com", "red bold italic"),
-			"\x1b]8;;https://google.com\a\x1b[31;1;3mHello\x1b]8;;\a\x1b[m")
+		input := termlink.ColorLink("Hello", "https://google.com", "red bold italic")
+		expected := "\x1b]8;;https://google.com\a\x1b[31;1;3mHello\x1b]8;;\a\x1b[m"
+		assert.Equal(t, input, expected)
 	} else {
-		assert.Equal(t, termlink.ColorLink("Hello", "https://google.com", "red bold italic"), "\x1b[31;1;3mHello (\u200bhttps://google.com)\x1b[m")
+		input := termlink.ColorLink("Hello", "https://google.com", "red bold italic")
+		expected := "\x1b[31;1;3mHello (\u200bhttps://google.com)\x1b[m"
+		assert.Equal(t, input, expected)
 	}
 }
