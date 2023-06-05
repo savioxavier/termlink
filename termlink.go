32fed242e9691b23207f587371e3b2f0d1325737
Skyascii <savioxavier112@gmail.com>
2023-06-05 14:06:52 +0400

    docs: improve documentation by 2000%

--- termlink.go
+++ termlink.go
@@ -7,18 +7,26 @@ import (
 	"strings"
 )
 
+// EnvironmentVariables represent the set of standalone environment variables
+// ie, those which do not require any special handling or version checking
 var EnvironmentVariables = []string{
 	"DOMTERM",
 	"WT_SESSION",
 	"KONSOLE_VERSION",
 }
 
+// Version struct represents a semver version (usually, with some exceptions)
+// with major, minor, and patch segments
 type Version struct {
 	major int
 	minor int
 	patch int
 }
 
+// parseVersion takes a string "version" number and returns
+// a Version struct with the major, minor, and patch
+// segments parsed from the string.
+// If a version number is not provided
 func parseVersion(version string) Version {
 	var major, minor, patch int
 	fmt.Sscanf(version, "%d.%d.%d", &major, &minor, &patch)
@@ -29,12 +37,16 @@ func parseVersion(version string) Version {
 	}
 }
 
+// hasEnvironmentVariables returns true if the environment variable "name"
+// is present in the environment, false otherwise
 func hasEnv(name string) bool {
 	_, envExists := os.LookupEnv(name)
 
 	return envExists
 }
 
+// checkAllEnvs returns true if any of the environment variables in the "vars"
+// string slice are actually present in the environment, false otherwise
 func checkAllEnvs(vars []string) bool {
 	for _, v := range vars {
 		if hasEnv(v) {
@@ -45,12 +57,15 @@ func checkAllEnvs(vars []string) bool {
 	return false
 }
 
+// getEnv returns the value of the environment variable, if it exists
 func getEnv(name string) string {
 	envValue, _ := os.LookupEnv(name)
 
 	return envValue
 }
 
+// matchesEnv returns true if the environment variable "name" matches any
+// of the given values in the "values" string slice, false otherwise
 func matchesEnv(name string, values []string) bool {
 	if hasEnv(name) {
 		for _, value := range values {
@@ -63,7 +78,7 @@ func matchesEnv(name string, values []string) bool {
 }
 
 func supportsHyperlinks() bool {
-	// Allow hyperlinks to be forced, even if this function
+	// Allow hyperlinks to be forced, independent of any environment variables
 	// Instead of checking whether it is equal to anything other than "0",
 	// a set of allowed values are provided, as something like
 	// FORCE_HYPERLINK="do-not-enable-it" wouldn't make sense if it returned true
@@ -71,18 +86,22 @@ func supportsHyperlinks() bool {
 		return true
 	}
 
+	// VTE-based terminals (Gnome Terminal, Guake, ROXTerm, etc)
+	// VTE_VERSION is rendered as four-digit version string
+	// eg: 0.52.2 => 5202
+	// parseVersion will parse it with a standalone major segment
+	// with minor and patch segments set to 0
+	// 0.50.0 (parsed as 5000) was supposed to support hyperlinks, but throws a segfault
+	// so we check if the "major" version is greater than 5000 (5000 exclusive)
 	if hasEnv("VTE_VERSION") {
-		// VTE-based terminals (Gnome Terminal, Guake, ROXTerm, etc)
-		// VTE_VERSION is rendered as four-digit version string
-		// eg: 0.52.2 => 5202
-		// parseVersion will parse it with a standalone major segment
-		// with minor and patch segments set to 0
-		// 0.50.0 (parsed as 5000) was supposed to support hyperlinks, but throws a segfault
-		// so we check if the "major" version is greater than 5000 (5000 exclusive)
 		v := parseVersion(getEnv("VTE_VERSION"))
 		return v.major > 5000
 	}
 
+	// Terminals which have a TERM_PROGRAM variable set
+	// This is the most versatile environment variable as it also provides another
+	// variable called TERM_PROGRAM_VERSION, which helps us to determine
+	// the exact version of the program, and allow for stricter variable checks
 	if hasEnv("TERM_PROGRAM") {
 		v := parseVersion(getEnv("TERM_PROGRAM_VERSION"))
 
@@ -99,14 +118,19 @@ func supportsHyperlinks() bool {
 		}
 	}
 
+	// Terminals which have a TERM variable set
 	if matchesEnv("TERM", []string{"xterm-kitty", "alacritty", "alacritty-direct"}) {
 		return true
 	}
 
+	// Terminals which have a COLORTERM variable set
 	if matchesEnv("COLORTERM", []string{"xfce4-terminal"}) {
 		return true
 	}
 
+	// Match standalone environment variables
+	// ie, those which do not require any special handling
+	// or version checking
 	if checkAllEnvs(EnvironmentVariables) {
 		return true
 	}
@@ -139,6 +163,7 @@ var colorsList = map[string]int{
 	"bgWhite":   47,
 }
 
+// isInList returns true if a given value is present in a string slice, false otherwise
 func isInList(list []string, value string) bool {
 	for _, v := range list {
 		if v == value {
@@ -150,12 +175,14 @@ func isInList(list []string, value string) bool {
 
 var colors []string
 
+// addColor adds a color to be later used while parsing the color string
 func addColor(value string) []string {
 	colors = append(colors, fmt.Sprint(colorsList[value]))
 
 	return colors
 }
 
+// isValidColor checks if a given color string is a valid key in a map of predefined colors
 func isValidColor(color string) bool {
 	// Create a slice with keys of the colorsList map
 	keys := make([]string, len(colorsList))
@@ -170,6 +197,8 @@ func isValidColor(color string) bool {
 	return isInList(keys, color)
 }
 
+// parseColor yields a set of ANSI color codes, based on the "color" string.
+// The color codes are parsed from the colorsList map
 func parseColor(color string) string {
 	// If nothing is provided, return empty string
 	if color == "" {
@@ -189,20 +218,24 @@ func parseColor(color string) string {
 	return "\u001b[" + strings.Join(colors, ";") + "m"
 }
 
-// Function Link creates a clickable link in the terminal's stdout.
+// Link returns a clickable link, which can be used in the terminal
 //
 // The function takes two required parameters: text and url
 // and one optional parameter: shouldForce
 //
 // The text parameter is the text to be displayed.
 // The url parameter is the URL to be opened when the link is clicked.
-// The shouldForce parameter indicates whether to force the non-hyperlink supported behavior (i.e., text (url))
+// The shouldForce is an optional parameter indicates whether to force the non-hyperlink supported behavior (i.e., text (url))
 //
 // The function returns the clickable link.
 func Link(text string, url string, shouldForce ...bool) string {
+
+	// Default shouldForce to false
 	shouldForceDefault := false
 
 	if len(shouldForce) > 0 {
+		// If a value for shouldForce is provided, set it to that instead
+		// Since shouldForce is a slice, we only consider its first element
 		shouldForceDefault = shouldForce[0]
 	}
 
@@ -216,7 +249,7 @@ func Link(text string, url string, shouldForce ...bool) string {
 	}
 }
 
-// Function LinkColor creates a colored clickable link in the terminal's stdout.
+// ColorLink returns a colored clickable link, which can be used in the terminal
 //
 // The function takes three required parameters: text, url and color
 // and one optional parameter: shouldForce
@@ -224,15 +257,18 @@ func Link(text string, url string, shouldForce ...bool) string {
 // The text parameter is the text to be displayed.
 // The url parameter is the URL to be opened when the link is clicked.
 // The color parameter is the color of the link.
-// The shouldForce parameter indicates whether to force the non-hyperlink supported behavior (i.e., text (url))
+// The shouldForce is an optional parameter indicates whether to force the non-hyperlink supported behavior (i.e., text (url))
 //
 // The function returns the clickable link.
 func ColorLink(text string, url string, color string, shouldForce ...bool) string {
 	textColor := parseColor(color)
 
+	// Default shouldForce to false
 	shouldForceDefault := false
 
 	if len(shouldForce) > 0 {
+		// If a value for shouldForce is provided, set it to that instead
+		// Since shouldForce is a slice, we only consider its first element
 		shouldForceDefault = shouldForce[0]
 	}
 
@@ -247,7 +283,7 @@ func ColorLink(text string, url string, color string, shouldForce ...bool) strin
 
 }
 
-// Function SupportsHyperlinks returns true if the terminal supports hyperlinks.
+// SupportsHyperlinks returns true if the terminal supports hyperlinks.
 //
 // The function returns true if the terminal supports hyperlinks, false otherwise.
 func SupportsHyperlinks() bool {
