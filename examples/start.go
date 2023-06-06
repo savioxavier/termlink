5a01e87432bffd678e67770323a5d0ae74bb4e76
Skyascii <savioxavier112@gmail.com>
2023-06-06 14:36:54 +0400

    chore(example): improve example code and add more links

--- examples/start.go
+++ examples/start.go
@@ -16,20 +16,40 @@ func printExample() {
 
 	fmt.Printf(
 		`
-Hello I'm %s.
-I'm %d years old.
-Here's Twitter: %s.
-And here's Twitter's Twitter: %s.
-Anyways, see ya!
+Here's an example to get you started:
+
+	Hello I'm %s.
+	I'm %d years old.
+	Here's Twitter: %s.
+	And here's Twitter's Twitter: %s.
 		`,
 		name,
 		age,
 		termlink.Link("Twitter", "https://twitter.com"),
-		termlink.ColorLink("@twitter", "https://twitter.com/twitter", "italic cyan"),
+		termlink.ColorLink("@twitter", "https://twitter.com/twitter", "italic blue"),
+	)
+}
+
+func printHelpLinks() {
+	fmt.Printf(
+		`
+Also, here are some other links to help you out:
+
+	🐹 %s
+	💻 %s
+
+	🔗 %s
+	💖 %s
+		`,
+		termlink.Link("Official Go Website", "https://go.dev"),
+		termlink.ColorLink("A Tour of Go", "https://go.dev/tour", "cyan"),
+		termlink.ColorLink("termlink's source code", "https://github.com/savioxavier/termlink", "italic magenta"),
+		termlink.ColorLink("termlink's donate page", "https://www.buymeacoffee.com/savioxavier", "italic red"),
 	)
 }
 
 func main() {
 	printSupportsHyperlinks()
 	printExample()
+	printHelpLinks()
 }
